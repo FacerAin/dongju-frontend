c740e2af148ea67b192a4d0374c6f1d2a92cfb80
FacerAin <ywsong.dev@kakao.com>
2021-10-03 02:07:35 +0000

    Update TextListContainer Filtering Function

--- app/src/containers/text/TextListContainer.tsx
+++ app/src/containers/text/TextListContainer.tsx
@@ -1,9 +1,19 @@
-import { FC, useEffect, useMemo } from "react";
+import { FC, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "modules";
 import TextList from "components/text/TextList";
 import { getTextAllAsync } from "modules/text";
 import Loading from "components/common/Loading";
+import { TextType } from "api/text";
+function filtering_data (
+  data: TextType[],
+  option: string,
+  searchWord: string
+) {
+  return data.filter((item) => {
+    return item[option].includes(searchWord);
+  });
+};
 
 const TextListContainer: FC = function () {
   const { data, loading, error } = useSelector(
@@ -19,20 +29,12 @@ const TextListContainer: FC = function () {
     dispatch(getTextAllAsync.request(""));
   }, [dispatch]);
 
-  const filtered_data = useMemo(() => {
-    if (data) {
-      let text_data = data;
-      return text_data.filter((item) => {
-        return item[option].includes(searchWord);
-      });
-    }
-  }, [data, option, searchWord]);
 
   return (
     <>
       {loading && <Loading />}
       {error && <p style={{ textAlign: "center" }}>Error!</p>}
-      {filtered_data && <TextList texts={filtered_data} />}
+      {data && <TextList texts={filtering_data(data, option, searchWord)} />}
     </>
   );
 };
